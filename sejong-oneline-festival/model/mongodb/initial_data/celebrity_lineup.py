f77796b3ec972a319fb7012e3b7ab21f56f8df6c
denhur62 <denhur62@naver.com>
2021-10-01 14:45:15 +0900

    셀러브레이트 데이터 수정

--- sejong-oneline-festival/model/mongodb/initial_data/celebrity_lineup.py
+++ sejong-oneline-festival/model/mongodb/initial_data/celebrity_lineup.py
@@ -1,18 +1,27 @@
+from datetime import datetime
 CELEBRITY_LINEUP = {
     'config_type': "celebrity_lineup",
     'celebrities': [
         {
             'name': "매드클라운",
             'datetime_info': "10.20 WED",
+            'banner_photos':"./app/asset/uploads/c1.png",
+            'youtube_link':"https://www.youtube.com/watch?v=2fFaD2uIf30",
+            "open_time": datetime.strptime("2021-10-19 14:21:57.86", '%Y-%m-%d %H:%M:%S.%f'),
         },
         {
             'name': "청하",
             'datetime_info': "10.21 TUR",
+            'banner_photos':"./app/asset/uploads/c2.png",
+            'youtube_link':"https://www.youtube.com/watch?v=900X9fDFLc4",
+            "open_time": datetime.strptime("2020-10-20 14:21:57.86", '%Y-%m-%d %H:%M:%S.%f'),
         },
         {
             'name': "장범준",
             'datetime_info': "10.21 TUR",
+            'banner_photos': "./app/asset/uploads/c3.png",
+            'youtube_link':"https://www.youtube.com/watch?v=BfWqUjunXXU",
+            "open_time": datetime.strptime("2021-10-20 14:21:57.86", '%Y-%m-%d %H:%M:%S.%f'),
         },
-    ],
-    'banner_photos': []
+    ]
 }
