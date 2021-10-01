c44e9ca430a5b5fb6987d0013664d740e303419f
denhur62 <denhur62@naver.com>
2021-10-02 05:34:23 +0900

    마지막 더미 데이터 추가

--- sejong-oneline-festival/model/mongodb/initial_data/comment.py
+++ sejong-oneline-festival/model/mongodb/initial_data/comment.py
@@ -30,6 +30,56 @@ COMMENTS = [
         "comment": "e-sport 관전 너무 기대 된다리~", 
         "content_id": ObjectId("61567c5699926a8b92dafbc4"), 
         "created_at": "2021-10-02 01:10:44"
+        },
+        {
+        "__version__": 1, 
+        "_id": "615735984a71bac2f9a9c951", 
+        "author_id": "16011101", 
+        "author_major": "컴퓨터공학과", 
+        "author_name": "금하연", 
+        "comment": "컴퓨터 공학과 주점에 환영 합니다 :)", 
+        "content_id": ObjectId("61567c5699926a8b92dafbc4"), 
+        "created_at": "2021-10-02 01:10:44"
+        },
+        {
+        "__version__": 1, 
+        "_id": "615735985a71bac2f9a9c951", 
+        "author_id": "16011200", 
+        "author_major": "호텔경영학과", 
+        "author_name": "이민영", 
+        "comment": "호경 와인 한 잔 하러 오세요~", 
+        "content_id": ObjectId("61567c5699926a8b92dafbc4"), 
+        "created_at": "2021-10-02 01:10:44"
+        },
+        {
+        "__version__": 1, 
+        "_id": "615735986a71bac2f9a9c951", 
+        "author_id": "17011100", 
+        "author_major": "인공지능학과", 
+        "author_name": "김동현", 
+        "comment": "알파고랑 한판 두실분~", 
+        "content_id": ObjectId("61567c5699926a8b92dafbc4"), 
+        "created_at": "2021-10-02 01:10:44"
+        },
+        {
+        "__version__": 1, 
+        "_id": "615735987a71bac2f9a9c951", 
+        "author_id": "18011100", 
+        "author_major": "컴퓨터공학과", 
+        "author_name": "박성실", 
+        "comment": "컴퓨터 공학과 주점은 어디인가요?", 
+        "content_id": ObjectId("61567c5699926a8b92dafbc4"), 
+        "created_at": "2021-10-02 01:10:44"
+        },
+        {
+        "__version__": 1, 
+        "_id": "615735998a71bac2f9a9c951", 
+        "author_id": "16011150", 
+        "author_major": "컴퓨터공학과", 
+        "author_name": "조성윤", 
+        "comment": "컴퓨터 공학과 롤 대전은 어디서 보나요?", 
+        "content_id": ObjectId("61567c5699926a8b92dafbc4"), 
+        "created_at": "2021-10-02 01:10:44"
         }
 
     ]
